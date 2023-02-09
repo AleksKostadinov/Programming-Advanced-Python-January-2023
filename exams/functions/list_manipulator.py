6108f46b0632829d58d5542dc4f8a273f551a445
AleksKostadinov <kostadinov12@gmail.com>
2023-02-09 18:04:39 +0200

    Exercises

---
+++ exams/functions/list_manipulator.py
@@ -0,0 +1,30 @@
+def list_manipulator(numbers_list, command, pos, *number: int):
+    if command == 'add' and pos == 'beginning':
+        for num in number[::-1]:
+            numbers_list.insert(0, num)
+    elif command == 'add' and pos == 'end':
+        for num in number:
+            numbers_list.append(num)
+    elif command == 'remove' and pos == 'beginning':
+        if number:
+            for num in number:
+                numbers_list = numbers_list[num:]
+        else:
+            numbers_list.pop(0)
+    elif command == 'remove' and pos == 'end':
+        if number:
+            for num in number:
+                numbers_list = numbers_list[:num-1]
+        else:
+            numbers_list.pop()
+    return numbers_list
+
+
+print(list_manipulator([1,2,3], "remove", "end"))
+print(list_manipulator([1,2,3], "remove", "beginning"))
+print(list_manipulator([1,2,3], "add", "beginning", 20))
+print(list_manipulator([1,2,3], "add", "end", 30))
+print(list_manipulator([1,2,3], "remove", "end", 2))
+print(list_manipulator([1,2,3], "remove", "beginning", 2))
+print(list_manipulator([1,2,3], "add", "beginning", 20, 30, 40))
+print(list_manipulator([1,2,3], "add", "end", 30, 40, 50))
